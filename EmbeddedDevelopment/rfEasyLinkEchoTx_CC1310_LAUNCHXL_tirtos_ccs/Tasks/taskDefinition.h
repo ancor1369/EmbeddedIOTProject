ba929e375e48e6992ef72bff17acc8b5e3b1037e
Andres Cordoba <ancor1369@gmail.com>
2020-02-21 22:49:35 -0500

    This advancement starts organizing the code to create the tasks we need to receive the information to start creating the labels

---
+++ EmbeddedDevelopment/rfEasyLinkEchoTx_CC1310_LAUNCHXL_tirtos_ccs/Tasks/taskDefinition.h
@@ -0,0 +1,10 @@
+
+
+/*
+ * @name : radioTask
+ * @Purpose: Manage the ussage of the radio interface of the microcontroller
+ * @return: Void
+ *
+ */
+
+void radioTaskFunction(void *arg0,void *arg1);
