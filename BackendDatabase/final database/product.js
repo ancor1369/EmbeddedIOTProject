4f064e4e93ab6bd65f8372240526a062eeb7818b
gurwinder13 <2050gurwinder@gmail.com>
2020-03-11 14:21:30 -0400

    product.js files test with postman

---
+++ BackendDatabase/final database/product.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 7000
+const testData = {
+  name: 'John Doe',
+  age: '30',
+  account_balance: '15.5'
+}
+app.get('/', (req, res) => res.send(testData))
+app.listen(port, () => console.log(`Server listening on port ${port}`))
