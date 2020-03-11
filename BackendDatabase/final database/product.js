dcbb836266db62035b866823ada522d67c2d222e
gurwinder13 <2050gurwinder@gmail.com>
2020-03-11 14:57:38 -0400

    product.js files test with postman

--- BackendDatabase/final database/product.js
+++ BackendDatabase/final database/product.js
@@ -2,9 +2,18 @@ const express = require('express')
 const app = express()
 const port = 7000
 const testData = {
-  name: 'John Doe',
-  age: '30',
-  account_balance: '15.5'
+    ProductID:'46',
+    Name:"mouse",
+    PriceDollar:'20',
+    PriceCents:'97',
+    Description:"wireless optical mouse",
+    URL:"https://bit.ly/largo1",
+    SKU:'4532324',
+    Model:"FM7544",
+    DueDate:'2018-11-22T05:00:00.000+00:00',
+    LayoutName:"RegularTag",
+    Update:"true",
+    Blink:"False"
 }
 app.get('/', (req, res) => res.send(testData))
 app.listen(port, () => console.log(`Server listening on port ${port}`))
