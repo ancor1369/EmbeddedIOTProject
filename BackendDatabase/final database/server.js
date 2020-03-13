d715bdd895f833d48dae75f251f67d827895a83a
gurwinder13 <2050gurwinder@gmail.com>
2020-03-13 16:17:56 -0400

    mongoose with node js and postman

--- BackendDatabase/final database/server.js
+++ BackendDatabase/final database/server.js
@@ -2,6 +2,160 @@ const express = require('express');
 const app = express(); 
 app.use(require("cors")()) 
 app.use(require('body-parser').json()) 
+const _ = require('lodash');
+var {devices} = require('./Models/product');
+var {mongoose} = require('./node_module/mongoose');
+const app = express();
+const port = 7000;
+
+app.use(function(req, res, next)
+{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+});
+
+app.get('/product',(req,res)=>{
+  console.log('getproduct');
+  devices.find({}).then((result)=>{
+      res.send(result);
+  });    
+});
+
+app.get('/product/:ProductID',(req,res)=>{
+  var id = req.params.SN;    
+  devices.find({ProductID:id}).then((result)=>{
+      res.send(result);
+  }).catch((err)=>{
+      res.send(err);
+  })
+
+});
+
+app.post('/product',(req,res)=>{
+  //If it can no create a new device, it will
+  //update the existing one    
+  console.log('postproduct');
+  var body = _.pick(req.body,['ProductID','Name','','priceDollar','pricecents',
+  'Model','blink','Update','LayoutName','SKU','URL','Description','DueDate']);    
+  var avl = false;
+  if(body.SKU === "000")
+  {
+      avl = true;
+  }   
+  var newprouct = new product({
+      
+      ProductID:body.ProductID,
+        
+      Name:body.Name,
+            
+        PriceDollar:body.PriceDollar,
+           
+        PriceCents:body.pricecents,
+          
+        Description:body.Description,
+            
+        URL:body.URL,
+           
+        SKU:body.SKU,
+           
+        Model:body.Model,
+            
+        DueDate:body.DueDate,
+            
+        LayoutName:body.LayoutName,
+           
+        Update:body.Update,
+           
+        Blink:body.blink,
+
+        Available: avl
+
+  });    
+  console.log(newprouct);
+  newproduct.save().then((result)=>{
+      res.send(result);
+  },(error)=>{
+      //res.status(400).send(error);
+      product.findOneAndUpdate({ProductID:body.ProductID},{
+          
+      ProductID:body.ProductID,
+        
+      Name:body.Name,
+            
+        PriceDollar:body.PriceDollar,
+           
+        PriceCents:body.pricecents,
+          
+        Description:body.Description,
+            
+        URL:body.URL,
+           
+        SKU:body.SKU,
+           
+        Model:body.Model,
+            
+        DueDate:body.DueDate,
+            
+        LayoutName:body.LayoutName,
+           
+        Update:body.Update,
+           
+        Blink:body.blink,
+
+        Available: false
+      }).then((result)=>{
+          console.log('result');
+          res.send(result);
+      }).catch((error)=>{
+          console.log('error');
+          res.send(error);
+      });
+  });
+});
+
+app.patch('/product',(req,res)=>{
+  //makes the update of the product
+  devices.findOneAndUpdate({ProductID:body.ProductID},{
+       
+    ProductID:body.ProductID,
+        
+    Name:body.Name,
+          
+      PriceDollar:body.PriceDollar,
+         
+      PriceCents:body.pricecents,
+        
+      Description:body.Description,
+          
+      URL:body.URL,
+         
+      SKU:body.SKU,
+         
+      Model:body.Model,
+          
+      DueDate:body.DueDate,
+          
+      LayoutName:body.LayoutName,
+         
+      Update:body.Update,
+         
+      Blink:body.blink,
+
+      Available: avl
+  }).then((result)=>{
+      console.log('result');
+      res.send(result);
+  }).catch((error)=>{
+      console.log('error');
+      res.send(error);
+  });
+});
+
+app.listen(port,()=> console.log(`Server started on port ${port}`));
+
 
 app.get("/", (req, res) => {
   res.send("get products") 
