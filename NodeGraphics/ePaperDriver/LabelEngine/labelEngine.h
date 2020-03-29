6f280d82cbc4a1228d92eed09910b7cb6424324c
Andres Cordoba <ancor1369@gmail.com>
2020-03-29 17:19:25 -0400

    This advancement organizes the project to make sure we have just what is needed

---
+++ NodeGraphics/ePaperDriver/LabelEngine/labelEngine.h
@@ -0,0 +1,9 @@
+
+
+/*
+ * This method is the only one I expose.
+ * This method takes the JSON object and
+ * then calculate the sections to call in
+ * order to draw the label as it is required
+ */
+void createLable(const char *object);
