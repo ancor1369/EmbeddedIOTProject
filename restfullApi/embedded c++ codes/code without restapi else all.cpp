a5da3ef73bd60b786f7ab92d4eefb57bd4831321
puneet kaur <55204433+puneetkpopli@users.noreply.github.com>
2020-04-01 11:17:10 -0400

    combined code

---
+++ restfullApi/embedded c++ codes/code without restapi else all.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+
+using namespace std:
+struct node*next;
+};
+structnode*head=NULL;
+void insert(intnew_data){
+    structnode*new_newnode=node(structnode*)
+malloc(sizeof(structnode));
+new_node data=new_data;     //kindly insert data here
+new_node next=head;   //kindly insert head or initial node details here
+head=new_node;
+
+}
+
+void display()    {     //displays whole linked list
+    structnode*ptr  // address of head 
+    ptr=head;
+    while(ptr!=null;)
+    {
+        cout<<ptr data<<"";  // kindly insert recieving data here 
+        ptr=ptr next;
+
+    }
+}
+int main(){
+    insert();   //kindly insert no. of elemnents here
+    cout<<"message"; // kindly put the message here
+    display();
+    
+
+
+}
+class dummy {
+    char name [****]  // kindly insert a char name 
+    public :// memeber function 
+    void setname (char*nam)
+    {strcpy(name,nam);   // copy function 
+    int main ()
+    {
+        char temp[***]   // kindly insert a name 
+        cout<<"name";
+        cin<<"temp";
+        cin<<"bool";
+        dummy_some object;
+        some_object.setname(temp);
+    }
+    }
+    }
+}
+template <typename T>   // for queue class
+class array{
+    private :
+    T*ptr;
+    int size;
+    public:
+    array(T arr[],int s)
+    void print ();
+
+}
+template <typename T>
+array<T> ::array (T arr[],int s);
+{
+    ptr=new T [s];
+    size=s;
+    for (int i=0;i<size; i++)
+    cout<"message"<*(ptr+i);   // kindly put a message here for array
+    cout <<end l;
+
+}
+int main ()
+{ int arr[]={ }}  // insert array data 
+
