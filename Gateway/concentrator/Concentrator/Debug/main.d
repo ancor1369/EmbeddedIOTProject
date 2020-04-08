587df3ea84e49c6ffadc5f8311a708a075080cf5
Andres Cordoba <ancor1369@gmail.com>
2020-04-08 16:12:31 -0400

    In this advancement I start the implementation of the Nlohmann JSON library to handle the incomming messages

--- Gateway/concentrator/Concentrator/Debug/main.d
+++ Gateway/concentrator/Concentrator/Debug/main.d
@@ -1,3 +1,10 @@
-main.o: ../main.cpp ../restDriver/restDriver.h
+main.o: ../main.cpp ../restDriver/restDriver.h ../nlohmann/json.hpp \
+ ../Models/MessageModels.h ../Models/../nlohmann/json.hpp
 
 ../restDriver/restDriver.h:
+
+../nlohmann/json.hpp:
+
+../Models/MessageModels.h:
+
+../Models/../nlohmann/json.hpp:
--- Gateway/concentrator/Concentrator/main.cpp
+++ Gateway/concentrator/Concentrator/main.cpp
@@ -1,10 +1,18 @@
 
 #include "restDriver/restDriver.h"
+#include "nlohmann/json.hpp"
+#include "Models/MessageModels.h"
+
+
+
+#define message "CA00{\"CXT\":\"PRO\",\"Object\":{\"DeviceID\":\"01\"}}"
+#define messages "{\"CXT\":\"PRO\",\"Object\":{\"DeviceID\":\"01\"}}"
+
+using json = nlohmann::json;
 
 
 int main()
 {
-
 	restDriver driver;
 
 	std::string server = "http://localhost:7000/";
@@ -14,7 +22,30 @@ int main()
 	driver.setEndPoint(endpoint);
 
 	std::cout << driver.getMethod() << std::endl;
+	std::cout<<message<<std::endl;
 
 
-	    return 0;
+	ms::incommingMessage p = {"PRO","{\"DeviceID\":\"01\"}"};
+
+	nlohmann::json jj = p;
+
+	std::cout << jj << std::endl;
+
+	std::cout<<"From created object"<<std::endl;
+	std::cout<<jj["Object"].get<std::string>()<<std::endl;
+
+	nlohmann::json internalObject = jj["Object"].get<std::string>();
+
+	auto inMessage = nlohmann::json::parse(messages);
+
+	std::cout<<"From parsed message"<<std::endl;
+	std::cout<<inMessage["Object"].get<nlohmann::json>()<<std::endl;
+
+	auto internal = inMessage["Object"].get<nlohmann::json>();
+	std::cout << internal["DeviceID"].get<std::string>()<<std::endl;
+
+	ms1::internalMessage inMsg = inMessage["Object"].get<ms1::internalMessage>();
+	std::cout<<inMsg.DeviceID<<std::endl;
+
+	return 0;
 }
