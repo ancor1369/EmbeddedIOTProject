aa38277fef853546ea1bd2e7e82c7d401074c18d
manpreet730931 <manpreetlaicha@gmail.com>
2020-04-11 23:40:40 -0400

    uarttxcode

--- restfullApi/arduino_serialdata_interface_program/arduino_serialdata_interface_program.ino
+++ restfullApi/arduino_serialdata_interface_program/arduino_serialdata_interface_program.ino
@@ -1,15 +1,12 @@
 int x=0;
 void setup() {
-  /*Inicializa a comunicao serial a uma 
-  taxa de 9600 baunds por segundo*/
+  
   Serial.begin(9600);
 }
 
 void loop() {
-  /*Serial.print("Contando: ");   //Imprime a palavra "Contando"
-  Serial.print(x);              //Imprime o valor da variavel x
-  Serial.println(" segundos."); //Imprime a palavra " segundos."*/
-  Serial.println("Contando: "+(String)x+" segundos.");
-  delay(1000);                  //Espera 1 segundo
-  x++;                          //Soma x+1
+ 
+  Serial.println("transmitter: "+(String)x+" reciever.");
+  delay(1000);                  
+                          
 }
