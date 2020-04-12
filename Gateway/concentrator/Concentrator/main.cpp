30154923b787f47409cc38b714ade994aded38c4
Andres Cordoba <ancor1369@gmail.com>
2020-04-11 21:48:38 -0400

    In this advancemet I started the implementation of the queue system I need to get the requests and handle all the requests. Next thing to do is to make sure that once a response is obtained, it is possbile to delete the message from the message list

--- Gateway/concentrator/Concentrator/main.cpp
+++ Gateway/concentrator/Concentrator/main.cpp
@@ -1,34 +1,38 @@
-#include "nlohmann/json.hpp"
+
 #include "Models/MessageModels.h"
 #include "Models/MessageRequest.h"
 #include "Models/Parser.h"
 
 #define message "CA00{\"CXT\":\"PRO\",\"Object\":{\"DeviceID\":\"01\"}}"
+#define message1 "CA00{\"CXT\":\"PRO\",\"Object\":{\"DeviceID\":\"02\"}}"
 
 using json = nlohmann::json;
 //This object will parse the raw incoming messages
 //and will allow me to reuse it as more and more
 //messages arrive.
 Parser parse;
+std::list<MessageRequest> lista;
+std::list<MessageRequest>::iterator it;
 
 int main()
 {
+	//Simulate how the messages arrive to the system, they are
+	//added to the container list and then the internal method is called
+	//to make the actual request
 	parse.setRawMessage(message);
+	lista.push_back(MessageRequest(parse.getParsedMsg(),parse.getSender()));
+	parse.setRawMessage(message1);
+	lista.push_back(MessageRequest(parse.getParsedMsg(),parse.getSender()));
 
-	//This section creates the object where I can save the messages
-	//So that I can handle the information in this way.
-
-	MessageRequest requester(parse.getParsedMsg(),parse.getSender());
-	requester.sendRequest();
-
-	std::cout << requester.getResponse()<<std::endl;
-	std::cout<< "This respose goes for the following device: " + requester.getSender() << std::endl;
-
-	//Starts implementation of the list to support all the incoming messages
-	//that need to be processed on this device
-	std::list<MessageRequest> lista;
-
-	lista.push_back(requester);
+	for(it = lista.begin(); it != lista.end(); it++)
+	{
+		it->sendRequest();
+		std::cout<<it->getResponse()<<std::endl;
+		if(it->getResponse() != "")
+		{
+			lista.erase(it);
+		}
+	}
 
 	return 0;
 }
