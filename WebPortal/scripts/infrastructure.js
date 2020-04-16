a53216977dc18e7322c1bc855cbffb9179e614bb
manpreet730931 <manpreetlaicha@gmail.com>
2020-04-16 01:12:34 -0400

    backend calling

--- WebPortal/scripts/infrastructure.js
+++ WebPortal/scripts/infrastructure.js
@@ -1,2 +1,14 @@
 loadProductsTable();
 // make a call to the backend database.
+const app = document.getElementById('content-wraper');
+const container = document.createElement('div');
+var btnCreate = document.createElement('button');
+btnCreate.setAttribute('type','button');
+btnCreate.setAttribute('class','btn btn-primary');
+btnCreate.setAttribute('id','btnCreate');
+btnCreate.textContent = 'Create Product';
+
+    app.append(btnCreate);
+    app.append(container);
+
+    loadProductsTable();
