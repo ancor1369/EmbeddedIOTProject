73f98f10a211e131ff56c9e98337a0cdd2b9946c
manpreet730931 <manpreetlaicha@gmail.com>
2020-04-17 20:02:10 -0400

    advancement in creating product

--- WebPortal/scripts/infrastructure.js
+++ WebPortal/scripts/infrastructure.js
@@ -14,6 +14,10 @@ var buttonCreate = document.getElementById('btnCreate');
 var CreateModal = document.getElementById('createModal');
 var CancelModal = document.getElementById('btnCancel');
 
+
+
+
+
 buttonCreate.onclick = function()
 {
     CreateModal.style.display = "block";
@@ -22,4 +26,32 @@ CancelModal.onclick = function()
 {
     CreateModal.style.display = "none";
 }
+// button id for creating a new product. 
+var CreateProduct = document.getElementsById('btnCreateProduct');
+CreateProduct.onclick = function()
+{
+    var prdID = document.getElementsById('txtProductID');
+    var prdName = document.getElementsById('txtProductName');
+    var prdPriceDollar= document.getElementsById('txtPriceDollar');
+    var prdPriceCent = document.getElementsById('txtPriceCent');
+    var prDescription = document.getElementsById('txtDescription');
+    var prdURL = document.getElementsById('txtURL');
+    var prdSKU = document.getElementsById('txtSKU');
+    var prdModel = document.getElementsById('txtModel');
+    var prdDueDate = document.getElementsById('txtDueDate');
+    var prdLayout = document.getElementsById('txtLayoutName');
+    var prdUpdate = document.getElementsById('txtUpdate');
+    var prdBlink = document.getElementsById('txtBlink');
+
+    if(prdID.value == ''|| prdName.value == ''|| prdPriceDollar.value == ''|| prdPriceCent.value == '' || prdDescription.value == ''||
+     prdUpdate.value == '' || prdUpdate.value == '' || prdBlink)
+     {
+
+        
+     } 
+    
+    
+}
+
+
 loadProductsTable();
